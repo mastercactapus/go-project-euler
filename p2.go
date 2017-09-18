8bae2ffd92080fd7833b9d7297cd67ac060a533b
Nathaniel Caza <mastercactapus@gmail.com>
2017-09-17 20:52:02 -0500

    solve 2

---
+++ p2.go
@@ -0,0 +1,29 @@
+package main
+
+// encrypt = ignore
+
+/*
+
+This file is encrypted.
+The key is the answer to the indicated problem.
+
+
+-----BEGIN ENCRYPTED CODE-----
+Nonce: DaDCMSVrOyAZtaoe
+Problem Number: 2
+Salt: zOoml+Y9QFJcRqvqqgLqfkABxQrIqWVA+StN7yG/T2k=
+
+daGVGRpYhScFnbYP4M65guc5VTkxhJsxJqY94SVgEd6hux56OV0KpZQuUkOtwdA2
+HTTwJRVoSBdKk1wX7jw1N6uBxiCNoXeNLjizcFjJRtfNvFvtFNdhTVGnNqsR+PkJ
+8INBsodC7oPbKv6ubgRJWMibZs2g/03h8Zpb81h24rzuDve0ztkQanF6+fE7Nct4
+cclup8VuER4yfKSJH9VZM2sP3NsbkMJMCDYKTwpe5KpfDYwIHpfHdUieiD7ZcHEv
+ayuLEkbRMOwMzACifPakvztz7KDutY2Tg3jW+y6G/ibyELKLEseM5+pipU7URgUg
+MMqXZNJ7zMXVh9bRseO5/q2GFYGot98OAGnsjfHW7k7zafVLfq08nyus0B/vffGI
+JSpMrm+cKumYndGUPJ6ED+6bs7pcFu1LCqUPRtmqwLTrOM2n5oKxNIB1Gtn/T4Bs
+FmPUGD2GdBAWwoK/73SKxfbJEBqUHcmXVv3KeSMSUkChfIUlvrux7YCiTzKBtmyP
+rm1dnu3pmZPr4+fOa4ONamadK9xujTv0O0IGxEfclFF93XDGf1vuhexvPdd68mpa
+rg3BaoqKC90Hv/97IFUWqjh5hU1vlNkUakCfu6eI4jIS2fBWtHd4UzZ8eacmq6pQ
+5W8tXHwj8gbCyv2nMCduOILW4HxaFgOpqf/1MCGQ5V2w1/HCB57+nydtkD4ctGQJ
+XoZ1vVfeZRwuIL2XzIlRthNIIQI4wWwvxxtqnciRHA==
+-----END ENCRYPTED CODE-----
+*/
