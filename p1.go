b538993e7c21866e706a26f90cd768bf530628d4
Nathaniel Caza <mastercactapus@gmail.com>
2017-09-17 20:39:56 -0500

    rename problem 1

---
+++ p1.go
@@ -0,0 +1,27 @@
+package main
+
+// encrypt = ignore
+
+/*
+
+This file is encrypted.
+The key is the answer to the indicated problem.
+
+
+-----BEGIN ENCRYPTED CODE-----
+Nonce: lWG1xLahy5J7KqCP
+Problem Number: 1
+Salt: DhN0S32c82kVmQmQ4nQVQ0sDoLWAgj+ZK6JmxunPqOI=
+
+iuMt04T0BOmCpeUfJybXd2HPkSgmxFs58NOGeeYDll+1fTeqnGJVs6deqpK5zPPq
+wSMDZA0taPPEmJG7VHy2vEx0c5m3B0i+JWJX45FUzBXTEHbbV0AZ2pV2rjAiKtif
+vjEL5UtFUG5ZqIq6+Qe6wOUKR7ubBgjUCKslfk7eC0ORjzSjDu0/AAuQMElbUiII
+lvmuVUZhp3CXbYH8vpys2W9YGGgQiiEd5TO9cPH7Jwxox1fLdVBPa2612lsske+2
+Fo+VHdl1VCqw9PebrUQw9YmP/lb/UAkp+tILbTx5nhxbP+VXJdTWgMrPuWCLJzeQ
+ZLnEy00yG1CvUI5B6HF9RYOtDCV8A1lCsXLTm9S6EtL4GU4DoYpByVZFycqac5en
+fTH/yAKTa62Sk2CTyted0eWg6yB3am3A4ymoScbjuCLB5Vn8XzXXfe5EzIc9mqMu
+zCXo1Kdi52TB6MIXlfbaZOCGTgQrLmEwsbsCocjq4ROPH2FNqMy6d97ATpfkSLFo
+mSgunw4kSe+zvHTjYr3SwEKMRPQXb1purMgCns1Cht71HKBhGko/WGDe+idrwDWm
+r4lJeg==
+-----END ENCRYPTED CODE-----
+*/
