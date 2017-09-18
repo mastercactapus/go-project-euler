8ed648eb9412b71eccb9b2bfadb741d64cf4e71c
Nathaniel Caza <mastercactapus@gmail.com>
2017-09-17 21:45:15 -0500

    solve 10

---
+++ p10.go
@@ -0,0 +1,25 @@
+package main
+
+// encrypt = ignore
+
+/*
+
+This file is encrypted.
+The key is the answer to the indicated problem.
+
+
+-----BEGIN ENCRYPTED CODE-----
+Nonce: HI4r/m94iCxxSfMJ
+Problem Number: 10
+Salt: QzaL29DWfYxAP+0Fw6aTbo8J55d33dCy9OSMEd/2JGA=
+
+af1fI4DQerGY2NHqLWszaZBm3qNLbXbTwqibnmSwLAO/b08jgjF0d+Z76esmCnGh
+086+xXPfGZuIMrwzzwc7o2okwAfOPAe/c+h7Lkaqd6xTOQDrz/uT35KeFihtNYmP
+w9gVDKgsqGtyZg6jBM6OKADCgYv7eoKDixQUJfEQ0LsmrvJ6nPSwFx6jMExLzmy4
+JTPeKhNKRojqv5WxqC4/OtsejJHFo7msRHv9Fw3M3o2Nd+PVOkbOt+5GnuhuDdEx
+BlE8ZHTVNsjtTYvd5+gT49lDFUjNW8CwMlN/lSeDU104PSb9OpQ+i0TT4L75+4Xh
+pEN2tavnN+FgmE8ARonIFJldShAWo6ek2GBPB1HdAHG7PB3Z3z9G3IQU+rVjcLSS
+NmF/LfHouHbiF/nn1PMxd6v3pat7hkrFP3ejsX8W1zWTEvjMMyw3RLeAu0NWqLlt
+4gvMXX3gpWnlzmoP
+-----END ENCRYPTED CODE-----
+*/
