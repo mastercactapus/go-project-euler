62ba27baecec0a59611f0c9d3940c743776bd34a
Nathaniel Caza <mastercactapus@gmail.com>
2017-09-17 21:39:26 -0500

    7 & 8

---
+++ matrix8.go
@@ -0,0 +1,20 @@
+package main
+
+// encrypt = ignore
+
+/*
+
+This file is encrypted.
+The key is the answer to the indicated problem.
+
+
+-----BEGIN ENCRYPTED CODE-----
+Nonce: VjJCGNqcSagZG5Ek
+Problem Number: 8
+Salt: 12HLrc8V92AJnrdtCaewz+fAh62J8Q4UNBh/X2R8X54=
+
+P4VDDtViTqM4RKFP0+Ij2RFK/YejuTxzZO/ku3wMNhnhp1hxMVPkw6EfByRW7iGS
+c0fQ7mq5KzG2qMAvk+LgpnIZSTKAcvTA4BqSW7lgdklUmkIcyK9C4eW4Xm96R4+n
+LnhA1HUD9sKi8tuzPrkK58fzGOWx5LZcZj5AoVUGCc7lYG9vBUrO4IoxPA==
+-----END ENCRYPTED CODE-----
+*/
---
+++ p7.go
@@ -0,0 +1,24 @@
+package main
+
+// encrypt = ignore
+
+/*
+
+This file is encrypted.
+The key is the answer to the indicated problem.
+
+
+-----BEGIN ENCRYPTED CODE-----
+Nonce: 7JYGqYlsIRCxLnYt
+Problem Number: 7
+Salt: EXerDJenAukVZaLIe0wb03toNs1gxlZc8HozZ9HXx6E=
+
+S1uIJKoN2joNbwZRROXvm4/Ia5qpF247sm0afIVOLms2CeAz4xFMRXJPDKhC17SR
+gh7b1BOHZeJqvOL5tLb+jZn+xSY3n7x7zdXWWxSfRd0OE2Ubx2ZEjg+pf24PXYoF
+X4jj0rX8RxWNYmuC2tew9k6BurCNOUqdI6mLAtxfPB1KBqg1WLDgwJ8TYK8r/114
+wZvdJUfVdO7vfSqk8bxB/Jmh4YYweUzgHn9u1GCEFTQMTMbsNr/pirAc+G3ABMqg
+XUOC9OkYgo/OGNIdUEKAVB0x8EN7Vs9KLNVjXZ4UIZ4S2kAO+5qaAZghS6X84zCu
+MunCvMwze3dcwZDxgo+09M82rklBWSa5aGcM5PgMh9uO9JX8I4UOtQBMcyO9eHKP
+IPhSW7h3hZlZbw0=
+-----END ENCRYPTED CODE-----
+*/
