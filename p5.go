96cc83dbdb1a135f87f094b8c5f14398a6061a09
Nathaniel Caza <mastercactapus@gmail.com>
2017-09-17 21:26:45 -0500

    solve 5

---
+++ p5.go
@@ -0,0 +1,28 @@
+package main
+
+// encrypt = ignore
+
+/*
+
+This file is encrypted.
+The key is the answer to the indicated problem.
+
+
+-----BEGIN ENCRYPTED CODE-----
+Nonce: 0gUf5TCXEhYu3QZT
+Problem Number: 5
+Salt: kqqLQRNB+NJQhKji5eqR5hG/5JrGwNpylOOfJq+fD4Q=
+
+mqGuRo1joDZFr90d1j6lbtRMb9b+KLUWSaJ0yPAFdYU/4PYnL5Y68WN6smOFsFd3
+Doe7+xZnRqfpQA0ff/NGRXCtGC8n0I8873cjI59utWRtz9fcq4rW47x5VW6ldmRA
+Y7C+c0Vz+m809C+Td1GHW2OPdH4+IOtZAFfo/7OxNMxPUGbn6dTuuiqsZSkFKH3k
+Y9xNZfF1XcbzSwJDO2yH6uO2mtodlpL2gzyCxQTfqt5VXC9N1o4UVYydzSMK2s8G
+KPyb8BPFks02U0V92yrDowsB1QRg4ZS9XJeYbO946wGceMz2REhQCkG8L+EUZaG/
+KDjo6VtBCec6x5GuFiFcYosYp5G3kM3U96Fxa71wdnSHOS9midxXokEZVTxkderO
+0XurW+gDRyUxWVRpzxSfPEqF6tSM9tyALorU0ECRu2vCcMFRcKdwXPZk0kgSbpoy
+4gRdRJCNLsLnzx8nm/Lkr4Ne12zg7vuzvv7PVFJv+LORggnQBQwd397N3LwPtO3z
+VbmPSLaVOkl93NZW9sP65guzkftAnk+/L7cKz66MbRLCZ5AAZ8BiOHbPdXAOya+u
+bflVIhvZHQBKUuMIozZPjmrsUm2Z0ZMqh6iljm2ho7fynIpb0ai7RRtsopEbZqKM
+H79/Nj8Nmp4yHJ0WJ8spZ5oocAr/r8pb6dAF6SiVZ/2DFOuSp2YtO2af
+-----END ENCRYPTED CODE-----
+*/
