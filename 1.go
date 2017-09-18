b6bb7d9336e8500595fd47073a2a360ecadd81db
Nathaniel Caza <mastercactapus@gmail.com>
2017-09-17 20:28:48 -0500

    solve problem 1

---
+++ 1.go
@@ -0,0 +1,27 @@
+package main
+
+// encrypt = none
+
+/*
+This file is encrypted.
+The key is the answer to the indicated problem.
+
+
+-----BEGIN ENCRYPTED CODE-----
+Nonce: DSYe1BoYa7Myelrd
+Problem Number: 1
+Salt: pyqEzCVeWIKBfqKHZCl+p1DuOm4biYoa0XvrzR8+GwE=
+
+kMfjZOFITUFuQwMBGdaqJmlaYXxnLCLDRfvLU7IzyAOotXpSssnmcvs3bl0GVogh
+Up8ohgkGqDiwhNG2NynMkN5WL1y/4JawSRvn/AlbgLlEKH7ZU/YOaE2DKT+G9z+k
+eo5t11jKekXBlm7YIA/mGrLhGgaPJWS32nq6lcz8GRjxyN/20UBlFQ9qVwIZvkO7
+Es3MWcS2Ht9IIWAxKbXm/XqMQEfXfmEViCHBxGYSIBa3SWceUsrp/ZT/w2i08uGc
+NSpp9pTGWUJfqmfu9kQWRgoedV9i/+M20wNJXDFN7juKQ6uN1OErrBNPJ3VgY+mR
+wJRNsFycEw11nljGL9cVHPJ/il3WvxdF/dLfRTINDXoQMcLVI152vuNrfb0dBH3v
+FhlwqydoiHZGt7rzMMMb1iV8jCbPBtEbtajTXI9FpE9cy1j8VB1ri2FnIaEyPFAI
+SoGU4Otz0MGQOhEV2I9m/9ANA4zRMe9VUWuYgPsex16BORfn4bvdBEsPCBlX0Kif
+mrazK2QEoNF4kFz0Bkb3xYHmZLbCHER39+jIR4h3Yf/bub59wlMCJCd2HExs/EL5
+7ofD
+-----END ENCRYPTED CODE-----
+
+*/
