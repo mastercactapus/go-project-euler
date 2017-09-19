f5b613b240962540f98cf33f4907e63cc550eaee
Nathaniel Caza <mastercactapus@gmail.com>
2017-09-18 20:36:47 -0500

    15

---
+++ numbers15.go
@@ -0,0 +1,22 @@
+package main
+
+// encrypt = ignore
+
+/*
+
+This file is encrypted.
+The key is the answer to the indicated problem.
+
+
+-----BEGIN ENCRYPTED CODE-----
+Nonce: HUcEcVOurIdg7o48
+Problem Number: 15
+Salt: Yb9iW7axwVTLqPFv1Nq3xKQoSZgfaA4sEI/daVosmFQ=
+
+scBpYbvRUKKULZuqo6xdGO4JcnRl6GgvXMFD1tu+1RBKC5N6KwRk77WMnwK+OdNJ
+M0b/4YyShXNMbEPo6o3d3Uh4Ddub2vJtzTAffc70cmeo8AaPqhTCFmsgS7LkQQPe
+PqxiULaMOUBTbHl5EOmP9D4cEZzO5Pzb2wViAxReoZDvxJwZtp0AtQLNtrKdHBPj
+NcpA94qbPraTnoBKmm1gRbr0qvWjsH4VQylMU57yKGPj0/H7h+QWY6iwBtTuFnQI
+3Yq8BR/dZkWN/gQz9OylW8lEnIB2MSwPYYk=
+-----END ENCRYPTED CODE-----
+*/
---
+++ p15.go
@@ -0,0 +1,27 @@
+package main
+
+// encrypt = ignore
+
+/*
+
+This file is encrypted.
+The key is the answer to the indicated problem.
+
+
+-----BEGIN ENCRYPTED CODE-----
+Nonce: nSNOq1Zdx/UcVsqg
+Problem Number: 15
+Salt: VcKcs2zQnqak06nH0kI4YvbfzXnTlfuEGLobBEt9mhc=
+
+U/3YRzFzSgcsN2q9L/FRGMWf4M1jH6/16Gibu2w+2lkAWQWoTWVjyjD8kFDpehZi
+Nxj8QxwWk3DaprmYu1nIdRgR/RcvgWQY5uw/6fcrIh6w+DDeG6Ae0YGS7g/jKc7q
+IejCaLVYAoXTvaXxQ1FIJndeTNVilwR7SbG3uCixIh7b7RtO/C1744QcaGi48+nJ
+Cfy2Zvtw+PuJ36siM8O5ymp8N4cCoiJQOndeHtWxzPQfYAwntncow30RUOeGIY7x
+aBVlAks2uZFQO0FCb2OWHsFUPh2t8ZojzB9r/1XmutXgmJSiHgHvgQpc7aaE9Vh6
+YRtLuBTqzBpU9SEsYf7FS8WD/cvgy+0+8tSQlug1g3yH4Vtux8e9/S0wHD6HLdSK
+GflnqewAXSAYkkpBKNcYkVa4CcvQL39/urncuXjbcTC8uXliFZMk7V6rMm6Oz7H3
+YqEomk8bi/uYtuN6iF5OzUm/bpGw21psNGYkXozpNNMF6S7h+3Vjx05NuemsnWLX
+v9O8lXWBtr2+FeuYN9xymygF+q/rZPFcGKO6gmTZT0f5LAqId5TUppJU+/Dw/6DN
+/YCvt4iC7YBmtvurn1HP14XSMJrbQ0RV9g2fwT4KTzAgcBWzbe9EZlNs
+-----END ENCRYPTED CODE-----
+*/
