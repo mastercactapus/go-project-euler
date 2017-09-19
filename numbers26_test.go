4f2c011d45a80e6af475636155bd1cd86d065c21
Nathaniel Caza <mastercactapus@gmail.com>
2017-09-18 21:43:21 -0500

    26

---
+++ numbers26_test.go
@@ -0,0 +1,30 @@
+package main
+
+// encrypt = ignore
+
+/*
+
+This file is encrypted.
+The key is the answer to the indicated problem.
+
+
+-----BEGIN ENCRYPTED CODE-----
+Nonce: CXRm4CsMHNVuhqlQ
+Problem Number: 26
+Salt: gnwtIt2gbpI27i7gWxc/cwej6f67bzoSu1QJMRbzULM=
+
+wTp28D68onVVp29HCraCidCqDxufA2B0qz8lYomefLtrujI8qmBXAoJLaEroFItC
+h6WRBJyC60RFDPMtiSir+qMjUx78hHMPPIB314VZPXUx5NwYfCGiOQuxp5blsAHS
+N8hPlYAZcLxPGWCYbYKbmSormVuMsrbc6bugE2FTF6vVzznmDKld3naSVYVDvshi
+o4P91DzD7q7Bu+AgLZEt/Ju7H7NQaqa8rX9qNJlAANMYbkhqMhR/3qQ+yXMlFX+5
+6AF7vlurgfki1poKYsfBtAkFpbOMwW9tifUmn7yXJplnQ/JP4863k+/CW5wPCvEl
+TNwmmgZTzakOfUVuiujZFgDBWAl1pMl7+BjQ6rfI8ZqrWOvsYrVzoH+njnHDubCd
+v6vCj1KoN1unTLC+co0mT3KwIXboP7+ZPEOXPASuLtCqBaWzyAjRfjuNknHt7M98
+fBX17IiBKvtN91TuHEaOxT40w/PXSnYWbz7tRSx/+LxSmnkofy8FYIRI7mUVc4gb
+2byaNph/05VyiNVYHXDsz2rCp2LhDUPGdoE+WlaU3ZdRY4UjjUxsRDwCah5MxSHG
+RJ3uXWJj4LHvo0KsyLSWIBozG3sJOekAIYBLE795iqqOL50RPWm8yYthHPBf+QbW
+ubNBjIrcMkz21gATs3k9oDLvU79RUhgp+hHm5p0HhII5WjSXXKx/hO0FXfk3wi8s
+3UDZpO0l2pHwlDqKNdGsk8VkE8IbVvrcS+gfQQdodtJIlynInAN7IFkHU+zG0thi
+9i2IaX3WtsIp
+-----END ENCRYPTED CODE-----
+*/
---
+++ p26.go
@@ -0,0 +1,38 @@
+package main
+
+// encrypt = ignore
+
+/*
+
+This file is encrypted.
+The key is the answer to the indicated problem.
+
+
+-----BEGIN ENCRYPTED CODE-----
+Nonce: bKIFsAsmxg0znUIV
+Problem Number: 26
+Salt: +t/q3aCF6EuXwmNG9UeO6LwgUOGdN7C3bxQX56WoLs8=
+
+AnhSv8O57LLrVdSwq7/d1/7IdsiOJvJ49MAGUxePul/2dVF1vDmt6nhEQeYK2V4n
+ynQDqUUFXYZpUryaiA5xS2l7RccGVwTbc9DUmRAjFI8Hs8+EjGoDPzFRtY2gl6YY
+T1nwQi2W/e9nqgVu/1vI+/J0JN/PlRLQfhE2gy9joTB/K9chlUGVojG133NPBtvW
+8bCdgvWhJybpRbTYYbRD8Uumg60RbehroHP0emKGmlqQLDI4JoG3REcrHF27m2yj
+wBiRR5WP8ot9H3EXy6g9ovQsknIpFpQnxpfAQszJb9iaPQTYUZ88idXO1FPdiO+u
+yIhvUrKQQlWtw2lF372k2gbwTBU51gG+FWl3rTwQvWSacsmacWCvS4B5zPJtlZ8a
+SHRy9lmwAhcsTv68UuCf+b2hgrzmebykEqyhf4mQzoS1hVVUCY2f1JRk008bCAEp
+pkPeb9Uky2x3whiiESZuME02HpLLRFe/WHAuNnmrI4OS5L4rgHyecxl+k+QNa+PN
+kZ84PiVT9ocTYkhJnTi9Nb1jDv8Gg5lnOkqJPKclN9a9mingm0gouSnGCqRrpL4z
+y23Af2Qscj64/CTUedWl/O002mHJHxKOnKRAa65a7qp7kwmXr7NHDlYYi3tlKK/p
+z2dWwFQO3pL4aEao0B4u181JbxLWaDXhdI7KfcOYvormk+xqDzVsAFyAbMgR1/b1
+73ViDgEcJUZ+oDKlsIfwAmzwdMJLAb4pygmU3j72H5shJ6rhNsTpaEGQAkGSPNN5
+hhkrUoVICAghSS6rS6n/PgM3+k0bn08giaO2SYP4jxhdk67UiO6e9GJCaXN02fxP
+9zrczXzLnDFcgRtc8cvsa0jXUmbzh7cOhv5ml/JKZFU+IqbTxm6tcohTRqBAioWD
+tgT1tEIEIkSv1jpxOqx1bTta2RTr5v1f7HzUnYCA2ymiBCWEep6tc/gADIz6TyWp
+npKwZ2yYKto8uJSdxmjxOqaWiDozit6X83Kr0jwPO9/NMwUp499OH+N/iWCJbkok
+1Nam5fln+gnvJQkT9JUbotx32sMZC88L5+lp2qB/1t9/R0JrzxsrwOxXPWKxwrvS
++LfCle+j48K6664Ccf5lobus6noXYiwm69Q3/ZlAfsS34ilqwY43MxwQ/4mau16z
+cyVPIpzB/wCojENg/9Ar2MuqExb7UjaPwn3V0Ti1QUYTGcP9MFB6H2+MJkjM9Hd3
+miyFySA+eXTzozy8itjEsbJbyzogb0adqvr0C0rYfaJ5QSFEQXuEPBQl60lmFT/V
+Fr8ouw==
+-----END ENCRYPTED CODE-----
+*/
