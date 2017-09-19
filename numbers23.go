a3cb13b5ca047650e0c2cf0a263cd53f0d771de1
Nathaniel Caza <mastercactapus@gmail.com>
2017-09-18 21:00:34 -0500

    23

---
+++ numbers23.go
@@ -0,0 +1,20 @@
+package main
+
+// encrypt = ignore
+
+/*
+
+This file is encrypted.
+The key is the answer to the indicated problem.
+
+
+-----BEGIN ENCRYPTED CODE-----
+Nonce: fdtzZ0m1mVNfA9qk
+Problem Number: 23
+Salt: Bvu7QpT8Hu0w0AlDnbqvgn6e9ySkn6hHxfya8Z3aNws=
+
+qxs5rMbBqe9FZy7QRZ7m/DJTnggYrRiyphtiximWtr35Dy9lPgz95JJedAr+k+/8
+eP5pMjR/BcNExSu4VxmzjprEtUSN87nCbIILa8YzTFqPPxPL7q/JJEm2DM2eO3ey
+ya1E55qFua4V5F8=
+-----END ENCRYPTED CODE-----
+*/
