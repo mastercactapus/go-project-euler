8fb54405a9c90e1ebe39a54899ea03a6fed139da
Nathaniel Caza <mastercactapus@gmail.com>
2017-09-18 20:32:37 -0500

    13

---
+++ p13.go
@@ -0,0 +1,25 @@
+package main
+
+// encrypt = ignore
+
+/*
+
+This file is encrypted.
+The key is the answer to the indicated problem.
+
+
+-----BEGIN ENCRYPTED CODE-----
+Nonce: Co3iOFYiT55vGIf3
+Problem Number: 13
+Salt: 1bv1VLmQdTXgZP+4R5iinkfjovDOTkdQ9ngEa+0lKhE=
+
+BQ1b/cSfmY1bvaWUCPdz2Knr8cs1/UfzR2UPL2vTuLRiKz9MJBn/ZJ8fBpRX6d+e
+SqR1ntnFHALbX7+CqmQGnNWl8oiu1vvj0R1dUI5CURNMXBQiRv/YXjh81JtoUwOy
+CAVniAiDtuPB7fYmnALlY/lYBe4aoeQpvT+0GuDEQ/7PDJe6EE7rjbczUxMKaB1z
+ODGMNGCIDAj/52Fm6112m7UG+k1G86N4HqpFs2lksxEkd9vMnQN60ybsnKXzfTq5
+hw1l1VBXi01KoHP6R0tsyAjFWchXdo1bb13B1geJ9Nw92rtzVJcVbmPTWNKT41GC
+oxQK0m2hXtTeV4LjmVncLrrrVBgo6C/pxjkb0QtnAGaXIg1j7z6ZYwGBSjLgTe+T
+CzGDSvyzqnSLGAxG/YoU7MDRb9rJ76AjuYVBtchzwoCgq2kzIARkwwxLH3Kp/ljg
+vH7kVMwJidU=
+-----END ENCRYPTED CODE-----
+*/
---
+++ util13.go
@@ -0,0 +1,25 @@
+package main
+
+// encrypt = ignore
+
+/*
+
+This file is encrypted.
+The key is the answer to the indicated problem.
+
+
+-----BEGIN ENCRYPTED CODE-----
+Nonce: Vd4jVPz8vrfEGXga
+Problem Number: 13
+Salt: K4fBeTJ7seOZEXU2pZfPJ+8LhISrHFDhNGmjcK9q4cQ=
+
+p+wPYG3ipPXWrJEHrdytBTCEAqRnoqUBBAi7zEb3/TQxJeCe5P0hc5Iq43gqrr9d
+nliIautHQh6NTooBFMty8d78SPO5kh51g4ZdFd1BgIeX+anZA9Nv2ZNaS0M5xZku
+tV7ughgMwhn8b3JsVh97RcXw5GGYzITHecNN4oj1xD+jRQyJ2j/Va6IhCiOC4re0
+gVUS9x2UXS2y1fiIIH+AFQya2QWKk8EAYZR1FczpohCMcQdt5bvSMGSP2nlavEh9
+M2uxRzRwwpN8pdzoQQb/8SAu28tLqte+4BfrIFPMQZDOYZm6Bp/t6XJH1EA0DdzF
+Y61Yc9gZs0Jl7NkvdUbFxlseONU5WKzy58vX74nqLhIj+cJ/aN1Au7RjeY2MJ05i
+VHVDnfBhhA0kGorkqDsthHWKUlsYAE8g+hmf4vlr/GYyU7TG0O2Ro5y26Mt0FyJE
+vuvQBVwKNeJoRvobhuhdITzZLHD4zrsKltCP9vmHeFD4BmfFJc3VDKNd3E5Z45A=
+-----END ENCRYPTED CODE-----
+*/
