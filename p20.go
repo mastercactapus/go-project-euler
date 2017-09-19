8155dc54e0c6225611b4017844d7654eac7275d6
Nathaniel Caza <mastercactapus@gmail.com>
2017-09-18 20:51:07 -0500

    20

---
+++ p20.go
@@ -0,0 +1,27 @@
+package main
+
+// encrypt = ignore
+
+/*
+
+This file is encrypted.
+The key is the answer to the indicated problem.
+
+
+-----BEGIN ENCRYPTED CODE-----
+Nonce: l7R5sLdjrWPZDDhX
+Problem Number: 20
+Salt: cTh8UxojeHwaAB3l1l689UThcSg5QKVi+Z98gSb1Yks=
+
+qU0+8e8YALHDS1o4bDsmfKVJvsp5SoHkzcEKwA6gOIOodrWFLaOCRftJuyBQC7YP
+jbLu7Q3Yp8Ka7LihfQEs4YFy8fnZG/1iMoy2Avm56gMdHy2hnskgXUzX2ghXO0Ay
+f4ZjTz6oNwwFKwq539ZTm/gXf0ZFeJGKuIARZzZEuAaGbm2oTUxynCjbTtk5qasg
+SMWV9WhKCwX5AA3gmicWYdChExwQJosz26DNUs/A58nPVuMkfrfwLWa8/IyrdnvH
+nNkaHb/QswGL0TpVma9g9Fu1NElhmEa08NCEt6opf+5MCxWufCKo3SkuOtxFeLQB
+1omENOJdOOgClsJeZkYrQcO3BKcxMpr5ZA5kar1oOYu5qscLqNVc0khax97bxwzG
+TZ2zLwpostnhkSy5HMCdX7jsWcin4YyvxolRJM0oeVbcNm3Cuap8N8b3BC39huRF
+kdNCUhoui5RBuDx7KQGnJ/t9lolEu0GIX1E+HbirPoefXzpzu2m99PImiazj0Vko
+JytsVAxFJX4QOxal8EGOXOqjGFbI0zXjz4yvvL9/1G005Pkbyog2b4RpqGkYd9P/
+41mcQsmFbmKD7uR6wU7Lw8XW
+-----END ENCRYPTED CODE-----
+*/
