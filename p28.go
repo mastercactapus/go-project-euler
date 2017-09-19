03c39584a81359501eefa34e667950082571a763
Nathaniel Caza <mastercactapus@gmail.com>
2017-09-18 22:15:24 -0500

    28

---
+++ p28.go
@@ -0,0 +1,30 @@
+package main
+
+// encrypt = ignore
+
+/*
+
+This file is encrypted.
+The key is the answer to the indicated problem.
+
+
+-----BEGIN ENCRYPTED CODE-----
+Nonce: s6xrnLwR7nU3ieV+
+Problem Number: 28
+Salt: KdFq2zCq4JGCe/TOtBpSCNgChrMsQqVa/FPPIysjC2Y=
+
+6AS1uYzV55vNFufvIt8+MR2AmcG58cxUl11CgGegoPI8ZLzGZig7SBm+GQj447gC
+Q18UwiD9tUI+wxK22qZ+0tJ3ETgMCSniinwM4ubgXQxd2pVamXha6Vgqf2WCt/qK
+XBKBW6XW/mYIwOj+Os+KL0eNYwjFpwKA4/O2Ln/v1jrskJbV5VaIsqejWzF8Gk1j
+8PgSxz/0+7B9j2LnXb5Q//KRyE40wR7WuVKOhqSvyEsNCBF9uEO0EHIlWWvR7cko
+JzsrT6Il7ARer6tImTvGN/MeJbDZSu8kLzDj0tZDcVpGmOBKcLZjcLT2Z5fSCzSv
+O8V7522rl6gJHlAhkwRUwG8CqF29YrgE/A0ToEGf5o5/15WdK6dp2imegE9fRQJ3
+kcnzj/IUBSvl73i3g3ZgDmYSwT5zQ3V8JXGiVETdxivdhdXoZgThG/M8eTO8V6xJ
+b1GGMS9+oi9+39HvF4lT04OgF0zJ7zvJRbh/PNYabNaoH9EhvMJEjN75Sx3V/qv9
+Tax8kYyecqWvcPFULZ6Pzn1AwjStGlo5ZE4jJQoOsGRiTWBEg05Gc0XZRX6G/rmv
+63b8cxIsQIhl6vU0mKonvlFDC/fV6W2OcftCxD0BoFK96LOYy+OCy/DV8iB493TG
+u450RueyIEKQnzdWHSIUbDQqoQb8HnAse/04bhyhP8c6mdiI+4jZ+72DhK/MldZY
+5PnKf2C+wXgRFp0O56vND63yMiKLexZxPQa0ycRFM32gradjyZ2N5juuC8tGx1JN
+M1DIgwjzagKzBZY9xfeB/xqJK7rQxwYVacmuD08=
+-----END ENCRYPTED CODE-----
+*/
